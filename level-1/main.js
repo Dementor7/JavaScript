1c7635c8a514f8222f11f364c88da8197b2a3ef2
Dementor7 <73396404+Dementor7@users.noreply.github.com>
2021-03-02 08:39:10 +0530

    Add files via upload

---
+++ level-1/main.js
@@ -0,0 +1,25 @@
+const clr = document.getElementById('color')
+const btn = document.getElementById('btn')
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+btn.addEventListener('click', () => {
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    clr.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+
+
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
+
+
+
+
